3ca2d9741668a19a9a252230cb4c9854e007c996
MariyaFSpb <116431687+MariyaFSpb@users.noreply.github.com>
2023-03-09 15:32:44 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -22,3 +22,21 @@ if len(set(itog)) == 1:
     print('Парам пам-пам')
 else:
     print('Пам парам')
+# Задание №2.
+# Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), 
+# которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру
+# строки и столбца. Аргументы num_rows и num_columns указывают число строк и 
+# столбцов таблицы, которые должны быть распечатаны
+# . Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля). 
+# Примечание: бинарной операцией называется любая операция, у которой ровно 
+# два аргумента, как, например, у операции умножения.
+def print_operation_table(operation, num_rows=6, nu_columns=6):
+    if operation(1,1)==2:
+        print(1,end='\t')
+
+    for row in range(1, num_rows+1):
+        for column in range(1, nu_columns+1):
+            if operation(1,1)==2:
+                column=column-1
+            print(operation(row,column), end='\t')
+        print()
